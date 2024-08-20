08ac621723e1761100316f345e9eae420b478411
manishyadav077 <136573039+manishyadav077@users.noreply.github.com>
2024-08-20 15:21:09 +0545

    new code added

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const app = require("express")();
 const server = require("http").createServer(app);
 
 const PORT = process.env.PORT || 5000
+const HOST = process.env.HOST || '0.0.0.0';
 
 const io = require("socket.io")(server, {
   cors: {
@@ -51,5 +52,5 @@ io.on("connection", (socket) => {
 //app.listen(5000, ()=> console.log("Server is active"))
 
 server.listen(PORT, () => {
-  console.log(`Server is listening at port ${PORT}...`);
+  console.log(`Server is listening at http://${HOST}:${PORT}...`);
 });
